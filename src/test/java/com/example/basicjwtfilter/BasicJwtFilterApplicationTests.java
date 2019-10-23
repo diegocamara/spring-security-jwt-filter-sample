27b65d9ad94aa96cdc44a088f3c6544bdcf22754
Diego Câmara <diego.luiz.b.camara@accenture.com>
2019-10-23 10:30:43 -0300

    Renaming test class

--- src/test/java/com/example/basicjwtfilter/BasicJwtFilterApplicationTests.java
+++ src/test/java/com/example/basicjwtfilter/BasicJwtFilterApplicationTests.java
@@ -4,10 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BasicjwtfilterApplicationTests {
+class BasicJwtFilterApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {}
 }
